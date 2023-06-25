f9f4599ed49a47907be96ac8698222d56174fa84
EshimovShaxzod <126951571+EshimovShaxzod@users.noreply.github.com>
2023-06-25 19:57:46 +0500

    click button

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    let circle = document.getElementById('body');
+    circle.style.background = 'white';
+    let heading = document.getElementById('h2');
+    h2.style.color = 'black';
+    let btn = document.getElementById('btn');
+    btn.style.background = '#80ff00b6'
+    let animation = document.getElementById('circle');
+    animation.style.left = '40px';
+    animation.style.left = '48px';
+}
+
+
